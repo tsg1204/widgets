82f68495fa935cf5bacd8eec84c768ec92199139
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-30 17:18:41 -0500

    event listener added to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect , useRef} from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+          if (ref.current && ref.current.contains(event.target)) {
+            return;
+          }
+          setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        }
+    }, []);
 
     const renderedOptions = options.map((option, index) => {
         if (option.value === selected.value ) return null;
@@ -14,18 +30,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     })
 
     return (
-        <div className="ui form">
+        <div className="ui form" ref={ref}>
             <div className="field">
                 <label className="label">Select a Color</label>
-            </div>
-            <div 
-                className={`ui selection dropdown ${open ? 'visible active' : ''}`} 
-                onClick={() => setOpen(!open)}
-            >
-               <i className="dropdown icon"></i> 
-               <div className="text">{selected.label}</div>
-               <div className={`menu ${open ? 'visible transition' : ''}`} >
-                    {renderedOptions}
+                <div 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`} 
+                    onClick={() => setOpen(!open)}
+                >
+                <i className="dropdown icon"></i> 
+                <div className="text">{selected.label}</div>
+                <div className={`menu ${open ? 'visible transition' : ''}`} >
+                        {renderedOptions}
+                    </div>
                 </div>
             </div>
         </div>
