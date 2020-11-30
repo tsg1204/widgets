b707185cd0d27d2ab59a59bb84144c93a868069c
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-30 16:00:28 -0500

    refactor search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,19 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
+    
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -13,27 +24,17 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
-                }
+                    srsearch: debouncedTerm,
+                },
             });
 
             setResults(data.query.search);
         };
-
-        if (term && !results.length) {
-            const timeoutId = setTimeout(()  => {
-                if (term) {
-                    search();
-                }
-            }, 1000);
-    
-            //cleanup
-            return () => {
-                clearTimeout(timeoutId);
-            }
+        if (debouncedTerm) {
+            search();
         }
-        
-    }, [term]);
+           
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
