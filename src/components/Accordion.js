251852fa1774631fdad4ef780fddd383d34fcd49
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-30 09:35:05 -0500

    style accordion comp and add render method

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-    return (
-        <div>{items.length}</div>
-    )
+    const renderedItems = items.map((item, i) => {
+        return (
+            <div key={i}>
+                <div className="title active">
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </div>
+        )
+    });
 
+    return <div className="ui styled accordion">
+        {renderedItems}
+    </div>
 }
 
 export default Accordion;
