014839e92f560fa2e08a0f3833bd58ba7f5d86fa
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-30 13:47:05 -0500

    expand the accordion added

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
     const [activeIndex, setActiveIndex] = useState(null);
+
     const handleTitleClick = (index) => {
         setActiveIndex(index);
     }
 
     const renderedItems = items.map((item, i) => {
+        const active = i === activeIndex ? 'active' : '';
+        
         return (
             <div key={i}>
                 <div 
-                    className="title active"
+                    className={`title ${active}`}
                     onClick={() => handleTitleClick(i)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </div>
